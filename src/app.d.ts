40d82812ad84c480f393ee233f60476ee1bcf42e
Raygen Rupe <raygenrrupe@gmail.com>
2026-03-21 01:18:46 -0600

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.64.9
      project name = source-ysws
      framework = svelte
      framework cli = sv@0.12.4
      package manager = pnpm@10.28.2
      wrangler = wrangler@4.75.0
      git = 2.53.0

---
+++ src/app.d.ts
@@ -0,0 +1,25 @@
+import type { User, Session } from 'better-auth/minimal';
+
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace App {
+        interface Platform {
+            env: Env
+            cf: CfProperties
+            ctx: ExecutionContext
+        }
+
+        interface Locals {
+			user?: User;
+			session?: Session;
+		}
+
+        // interface Error {}
+        // interface PageData {}
+        // interface PageState {}
+        // interface Platform {}
+    }
+}
+
+export {};
