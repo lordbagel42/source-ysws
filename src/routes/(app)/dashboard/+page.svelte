f69c1349ff8e24847fab92786e4e8fbc2b18939d
lordbagel <raygenrrupe@gmail.com>
2026-03-22 19:19:00 +0000

    feat: add dashboard page at /dashboard route

---
+++ src/routes/(app)/dashboard/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import HeroSection from '$lib/components/site/mission/HeroSection.svelte';
+	import StreakHeatSection from '$lib/components/site/mission/StreakHeatSection.svelte';
+	import RecentShipmentsSection from '$lib/components/site/mission/RecentShipmentsSection.svelte';
+	import RewardLogSection from '$lib/components/site/mission/RewardLogSection.svelte';
+	import TerminalWindow from '$lib/components/site/mission/TerminalWindow.svelte';
+</script>
+
+<HeroSection />
+<StreakHeatSection />
+<RecentShipmentsSection />
+<RewardLogSection />
+<TerminalWindow />
