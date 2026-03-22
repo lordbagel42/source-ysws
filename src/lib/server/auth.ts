47e9bf72da072924e18d05da82072b545882ed98
lordbagel <raygenrrupe@gmail.com>
2026-03-22 18:01:43 +0000

    feat: set up better-auth infrastructure

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -12,8 +12,8 @@ export const auth = betterAuth({
 	emailAndPassword: { enabled: true },
 	socialProviders: {
 		github: {
-			clientId: env.GITHUB_CLIENT_ID,
-			clientSecret: env.GITHUB_CLIENT_SECRET
+			clientId: env.GITHUB_CLIENT_ID as string,
+			clientSecret: env.GITHUB_CLIENT_SECRET as string
 		}
 	},
 	plugins: [sveltekitCookies(getRequestEvent)] // make sure this is the last plugin in the array
